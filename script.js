394abb0ce650c8001382e252f25c3e173315e59e
rahadityaputra <rahadit.a.p123@gmail.com>
2024-04-05 16:23:46 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,91 @@
+function getDataFilm(url, succes, error) {
+    const ajax = new XMLHttpRequest();
+
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+            if (ajax.status === 200) {
+                succes(this.response);
+            } else if (ajax.status === 404) {
+                error();
+            }
+        }
+    };
+
+    ajax.open("GET", url);
+    ajax.send();
+}
+
+
+function searchFilm(input, getDataFilm) {
+	if (input.length > 3) {
+		// variabel url digunakan untuk menampung link yang sudah dibuat berdasarkan nama film.
+		const url = urlFilm(input);
+		getDataFilm(
+			url,
+			(response) => {
+				const dataFilm = JSON.parse(response);
+				let kumpulanFilm = ``;
+				dataFilm.Search.forEach((film) => {
+					kumpulanFilm += `<div class="col col-sm-6 col-md-4 col-lg-3">
+                                    <div class="card p-3" >
+                                        <img src="${film.Poster}" class="card-img-top" />
+                                        <div class="card-body">
+                                            <h5 class="card-title">${film.Title}</h5>
+                                            <p class="card-text">Tahun : ${film.Year}</p>
+                                            <a data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-primary btn-detail" data-id = "${film.imdbID}">Lihat Detail</a>
+                                        </div>
+                                    </div>
+                                </div>`;
+				});
+				document.querySelector(".film-box").innerHTML = kumpulanFilm;
+
+				// ketika tombol detail klik
+
+				const btnDetail = document.querySelectorAll(".card .btn-detail");
+				for (let i = 0; i < btnDetail.length; i++) {
+					btnDetail[i].addEventListener("click", function () {
+						document.querySelector(".detail-film").innerHTML = "";
+						// mengambil data id setiap tombol yang diklik
+						let imdbID = this.dataset.id;
+						const url = `http://www.omdbapi.com/?apikey=65b5aaad&i=${imdbID}`;
+						getDataFilm(url, (response) => {
+							const detailFilm = JSON.parse(response);
+							document.querySelector(".detail-film").innerHTML = `<div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">${detailFilm.Title}</h1>
+                                        </div>
+                                        <div class="modal-body">Sinopsis : ${detailFilm.Plot}</div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        </div>`;
+						});
+					});
+				}
+			},
+			() => console.log("pengambilan data film eror")
+		);
+	} else {
+		getMessageError();
+	}
+}
+
+
+// variabel untuk menampung url sesuai nama yang diinputkan user
+const userInput = document.querySelector(".input-film");
+
+function urlFilm(nama) {
+	return `http://www.omdbapi.com/?apikey=65b5aaad&s=${nama}`;
+}
+
+const formWrapper = document.querySelector(".form-wrapper");
+function getMessageError() {
+	const boxError = document.createElement("div");
+	boxError.classList.add("alert", "alert-danger");
+	boxError.setAttribute("role", "alert");
+	boxError.innerText = `Nama film yang anda masukkan kurang spesifik !!!`;
+	formWrapper.appendChild(boxError);
+
+	setTimeout(() => {
+		boxError.classList.add("hide-alert");
+		formWrapper.appendChild(boxError);
+	}, 1000);
+}
